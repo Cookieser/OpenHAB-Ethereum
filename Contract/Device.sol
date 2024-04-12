123a4abc1ea83443bb06490925d261525de869f5
Yupu Wang <87306014+Cookieser@users.noreply.github.com>
2024-04-12 20:04:50 +0100

    Update Device.sol

--- Contract/Device.sol
+++ Contract/Device.sol
@@ -1,39 +1,49 @@
 pragma solidity ^0.4.13;
 
 contract DeviceStore {
- enum DeviceStatus {Open, Close}
+    // Enum for device status
+    enum DeviceStatus { Open, Close }
 
- uint public deviceIndex;
- mapping (address => mapping(uint => Device)) stores;
- mapping (uint => address) deviceIdInStore;
+    // Public counter to keep track of the number of devices
+    uint public deviceIndex;
 
- struct Device {
-  uint id;
-  string name;
-  string category;
-  string imageLink;
-  string descLink;
-  uint deviceStartTime;
-  DeviceStatus status;
- }
+    // Mapping from store owner address to another mapping from device ID to Device struct
+    mapping (address => mapping(uint => Device)) stores;
 
- constructor() public {
-  deviceIndex = 0;
- }
+    // Mapping from device ID to store owner address
+    mapping (uint => address) deviceIdInStore;
 
- function addDeviceToStore(string _name, string _category, string _imageLink, string _descLink, uint _deviceStartTime) public {
-  //require ();
-  deviceIndex += 1;
-  Device memory device = Device(deviceIndex, _name, _category, _imageLink, _descLink, _deviceStartTime,DeviceStatus.Open);
-  stores[msg.sender][deviceIndex] = device;
-  deviceIdInStore[deviceIndex] = msg.sender;
-}
+    // Structure to store device details
+    struct Device {
+        uint id;
+        string name;
+        string category;
+        string imageLink;
+        string descLink;
+        uint deviceStartTime;
+        DeviceStatus status;
+    }
 
+    // Constructor to initialize the device index
+    constructor() public {
+        deviceIndex = 0;
+    }
 
-function getDevice(uint _deviceId) view public returns (uint, string, string, string, string, uint, DeviceStatus) {
-  Device memory device = stores[deviceIdInStore[_deviceId]][_deviceId];
-  return (device.id, device.name, device.category, device.imageLink, device.descLink, device.deviceStartTime, device.status);
-}
+    // Function to add a new device to the store
+    function addDeviceToStore(string _name, string _category, string _imageLink, string _descLink, uint _deviceStartTime) public {
+        // Increment the device index to ensure a unique ID for each new device
+        deviceIndex += 1;
 
+        // Create new device struct and store it in the mapping
+        Device memory device = Device(deviceIndex, _name, _category, _imageLink, _descLink, _deviceStartTime, DeviceStatus.Open);
+        stores[msg.sender][deviceIndex] = device;
+        deviceIdInStore[deviceIndex] = msg.sender;
+    }
 
+    // Function to retrieve device details by device ID
+    function getDevice(uint _deviceId) public view returns (uint, string, string, string, string, uint, DeviceStatus) {
+        // Fetch the device from the mapping using the device ID
+        Device memory device = stores[deviceIdInStore[_deviceId]][_deviceId];
+        return (device.id, device.name, device.category, device.imageLink, device.descLink, device.deviceStartTime, device.status);
+    }
 }
